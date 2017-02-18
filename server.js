61b610b67b53fbcf1dbb9d15ef8ad3a55c59db65
Chris Moscoso <christophermoscoso@gmail.com>
2017-02-18 01:17:24 -0500

    heroku mongo lab config

--- server.js
+++ server.js
@@ -1,55 +1,57 @@
 var express = require('express'),
-  stylus = require('stylus'),
-  logger = require('morgan'),
-  bodyParser = require('body-parser'),
-  mongoose = require('mongoose');
+	stylus = require('stylus'),
+	logger = require('morgan'),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
 function compile(str, path) {
-  return stylus(str).set('filename', path);
+	return stylus(str).set('filename', path);
 }
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(bodyParser.json());
-app.use(stylus.middleware(
-  {
-    src: __dirname + '/public',
-    compile: compile
-  }
-));
+app.use(stylus.middleware({
+	src: __dirname + '/public',
+	compile: compile
+}));
 app.use(express.static(__dirname + '/public'));
 
-if(env === 'development') {
-  mongoose.connect('mongodb://localhost/multivision');
+if (env === 'development') {
+	mongoose.connect('mongodb://localhost/Deep Scan');
 } else {
-  mongoose.connect('mongodb://jeames:multivision@ds053178.mongolab.com:53178/multivision');
+	mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds153719.mlab.com:53719/deepscan');
 }
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
-  console.log('multivision db opened');
+	console.log('deepscan db opened');
 });
-var messageSchema = mongoose.Schema({message: String});
+var messageSchema = mongoose.Schema({
+	message: String
+});
 var Message = mongoose.model('Message', messageSchema);
 var mongoMessage;
-Message.findOne().exec(function(err, messageDoc) {
-  mongoMessage = messageDoc.message;
+Message.findOne().exec(function (err, messageDoc) {
+	mongoMessage = messageDoc.message;
 });
 
-app.get('/partials/:partialPath', function(req, res) {
-    res.render('partials/' + req.params.partialPath);
+app.get('/partials/:partialPath', function (req, res) {
+	res.render('partials/' + req.params.partialPath);
 });
 
-app.get('*', function(req, res) {
-  res.render('index', {
-    mongoMessage: mongoMessage
-  });
+app.get('*', function (req, res) {
+	res.render('index', {
+		mongoMessage: mongoMessage
+	});
 });
 
 var port = process.env.PORT || 3030;
